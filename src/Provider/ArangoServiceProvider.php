cf75d388ab52eebbb823db7dfa3241d7b8451b8c
felipebastosweb <felipebastosweb@gmail.com>
2016-09-12 12:23:31 -0300

    initial silex and arangodb configuration

---
+++ src/Provider/ArangoServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace FelipeBastosWeb\Provider;
+
+//references:
+//https://www.arangodb.com/tutorials/tutorial-php/
+//https://github.com/helderjs/ArangoDbSilexServiceProvider
+//https://github.com/arangodb/arangodb-php
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+use triagens\ArangoDb\Conection;
+use triagens\ArangoDb\ConnectionOptions;
+use triagens\ArangoDb\UpdatePolicy;
+
+class ArangoServiceProvider implements ServiceProviderInterface {
+
+    public function register(Application $app)
+    {
+        $app['odm.arangodb.options'] = [
+	    ConnectionOptions::OPTION_ENDPOINT => 'tcp://127.0.0.1:8529',
+	    ConnectionOptions::OPTION_AUTH_TYPE => 'Basic',
+            ConnectionOptions::OPTION_AUTH_USER => 'root',
+            ConnectionOptions::OPTION_AUTH_PASSWD => '',
+            ConnectionOptions::OPTION_CONNECTION => 'Close',
+	    ConnectionOptions::OPTION_TIMEOUT => 3,
+	    ConnectionOptions::OPTION_RECONNECT => true,
+	    ConnectionOptions::OPTION_UPDATE_POLICY => UpdatePolicy::LAST
+	];
+	
+	$app['odm.arangodb.conn'] = new Connection($app['odm.arangodb.options']);
+    }
+
+    public function boot(Application $app)
+    {
+    
+    }
+
+}
---
+++ web/index.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('display_errors', 0);
+
+require __DIR__.'/../vendor/autoload.php';
+
+require __DIR__.'/../config/bootstrap.php';
+
+$app->run();
